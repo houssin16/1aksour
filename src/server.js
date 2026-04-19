5b3e4439e0c7eadc189a81cfa54c07c97c291bf2
houssin16 <houssinabaz@gmail.com>
2026-04-19 22:23:29 +0100

    Backend

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.options("/*", cors()); */
 
  app.use(cors());
-app.options('/*', cors());
+app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
