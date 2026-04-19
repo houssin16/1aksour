294b71a219e4459138b8674afb15b5a918667a5e
houssin16 <houssinabaz@gmail.com>
2026-04-19 21:21:02 +0100

    Fixedeee12

--- src/server.js
+++ src/server.js
@@ -6,8 +6,21 @@ const methodOverride = require("method-override");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const cors = require("cors");
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+/* const cors = require("cors"); */
+/* 
 app.use(cors({
   origin: [
     "http://localhost:3000",
@@ -16,7 +29,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-
+ */
 // middlewares
 
 app.use(express.json());
