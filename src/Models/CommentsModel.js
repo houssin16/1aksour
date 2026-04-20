486915c5372d133dfe5990f125fec99b5e06e591
houssin16 <houssinabaz@gmail.com>
2026-04-20 15:59:20 +0100

    fixed Mongodb coments Timestamps

--- src/Models/CommentsModel.js
+++ src/Models/CommentsModel.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const CommentSchema = new mongoose.Schema({
   postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  text: String
-   ({timestamps:true})
-});
+  text: String,
+  
+}, {timestamps:true})
 
 module.exports = mongoose.model("Comment", CommentSchema);
