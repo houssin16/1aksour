fc0dfc5bea4a2f937f0929e8cc648a4642205032
houssin16 <houssinabaz@gmail.com>
2026-09-03 23:07:28 +0100

    update 2026/08/18

--- src/Models/UsersModel.js
+++ src/Models/UsersModel.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  password: String,
   avatar: { type: String, default: "" },
-  coverImage: { type: String, default: "" }
+  coverImage: { type: String, default: "" },
 });
 
 module.exports = mongoose.model("User", UserSchema);
